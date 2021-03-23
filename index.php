9ddf4e562edfa71aa4ae673243f7ca676399b34b
Vlera Zhubi <vlerazhubi@jointdots.com>
2021-03-23 16:55:00 +0100

    changes

---
+++ index.php
@@ -0,0 +1,402 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick.css">
+    <link rel="stylesheet" type="text/css" href="./slick/slick-theme.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="container">
+              <a class="navbar-brand" href="#">
+                <img src="img/logo.png" class="d-inline-block align-top">
+              </a>
+              <ul class="nav ">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Shop</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Blog</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Accessories</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+              <div class="icons">
+                  <img src="img/search_icon.png" alt="">
+                  <img src="img/heart_icon.png" alt="">
+                  <img src="img/cart_icon.png" alt="">
+              </div>
+            </div>
+          </nav>
+          <div class="banner">
+                <div class="container">
+                    <div class="row text-row">
+                        <div class="col-xl-6">
+                            <div class="line"></div>
+                            <span class="white-txt">Welcome to </span><span class="yellow-txt">Pahu Design</span>
+                            <h1>You are in good hand with us</h1>
+                        </div>
+                    </div>
+                </div>
+          </div>
+    </header>
+    <main>
+      <div class="first-div">
+        <div class="container">
+          <div class="row title-row">
+            <div class="col-xl-12 text-center">
+              <h2>why choose us</h2>
+              <div class="line"></div>
+              <div class="rhombus"></div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="row content-row">
+            <div class="col-xl-4 text-right">
+              <div class="row">
+                <div class="col-xl-9">
+                  <h3>modern design</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="col-xl-3">
+                  <img src="img/icon1.png" alt="">
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xl-9">
+                  <h3>unique quality</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+                <div class="col-xl-3">
+                  <img src="img/icon2.png" alt="">
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-4 text-center ">
+              <img src="img/furniture.png" alt="">
+            </div>
+            <div class="col-xl-4">
+              <div class="row">
+                <div class="col-xl-3">
+                  <img src="img/icon3.png" alt="">
+                </div>
+                <div class="col-xl-9">
+                  <h3>Honest Prices</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-xl-3">
+                  <img src="img/icon4.png" alt="">
+                </div>
+                <div class="col-xl-9">
+                  <h3>Great support</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="second-div">
+        <div class="row no-gutters">
+          <div class="col-xl-3 banner-1">
+              <h4>Patio furniture vietnam</h4>
+              <a href="#">view detail -></a>
+          </div>
+          <div class="col-xl-3 banner-2">
+          </div>
+          <div class="col-xl-6 banner-3">
+            <h4>Design by jarnet park trend 2017</h4>
+            <a href="#">view detail -></a>
+          </div>
+        </div>
+        <div class="row g-0">
+          <div class="col-xl-3 banner-4"></div>
+          <div class="col-xl-6 banner-5">
+            <h4>CERAMICS WM MUNOZ</h4>
+            <a href="#">view detail -></a>
+          </div>
+          <div class="col-xl-3 banner-6"></div>
+        </div>
+      </div>
+      <div class="third-div">
+        <div class="container">`
+          <div class="row title-row">
+            <div class="col-xl-12 text-center">
+              <h2>Our collection</h2>
+              <div class="line"></div>
+              <div class="rhombus"></div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="row card-row">
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product1.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+                <div id="triangle-topleft"><p>new</p></div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product2.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product3.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product4.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product5.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product2.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product4.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-3 ">
+              <div class="card">
+                <img src="img/product1.png" alt="" class="card-img-top">
+                <div class="card-body">
+                  <h5 class="card-title">Corona Chair</h5>
+                  <p class="card-text">Printed evening dress with straight sleeves
+                    with black thin waist belt.</p>
+                  <p class="product-price">$14.86</p>
+                </div>
+                <div class="card-hover">
+                  <div class="icon-div">
+                    <div class="icons">
+                      <img src="img/search_icon.png" alt="">
+                      <img src="img/heart_icon.png" alt="">
+                      <img src="img/cart_icon.png" alt="">
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="forth-div">
+        <div class="container">
+          <div class="row title-row">
+            <div class="col-xl-12 text-center">
+              <h2>our designer  </h2>
+              <div class="line"></div>
+              <div class="rhombus"></div>
+              <div class="line"></div>
+            </div>
+          </div>
+          <div class="row designers-row">
+            <div class="col-xl-4">
+              <img src="img/designer1.png" alt="Bertil Stam">
+              <span>Bertil Stam </span>
+              <div class="line"></div>
+            </div>
+            <div class="col-xl-4">
+              <img src="img/designer2.png" alt="Bertil Stam">
+              <span>Bertil Stam</span>
+              <div class="line"></div>
+            </div>
+            <div class="col-xl-4">
+              <img src="img/designer3.png" alt="Bertil Stam">
+              <span>Bertil Stam</span>
+              <div class="line"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="fifth-div">
+        <div class="container">
+          <div class="row title-row">
+            <div class="col-xl-12 text-center">
+              <h2>our projects</h2>
+              <div class="line"></div>
+              <div class="rhombus"></div>
+              <div class="line"></div>
+            </div>
+          </div>
+        </div>
+        <div class="regular slider">
+            <div class="slides">
+              <img src="img/carousel-img1.png" alt="">
+              <h3>Modern Living Room</h3>
+              <p>interior design</p>
+            </div>
+            <div class="slides">
+              <img src="img/carousel-img2.png" alt="">
+              <h3>Modern Living Room</h3>
+              <p>interior design</p>
+            </div>
+            <div class="slides">
+              <img src="img/carousel-img3.png" alt="">
+              <h3>Modern Living Room</h3>
+              <p>interior design</p>
+            </div>
+            <div class="slides">
+              <img src="img/carousel-img4.png" alt="">
+              <h3>Modern Living Room</h3>
+              <p>interior design</p>
+            </div>
+            <div class="slides">
+              <img src="img/carousel-img4.png" alt="">
+              <h3>Modern Living Room</h3>
+              <p>interior design</p>
+            </div>
+            </div>
+      </div>
+    </main>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="./slick/slick.js" type="text/javascript" charset="utf-8"></script>
+  
+    <script type="text/javascript">
+      $(document).ready(function(){
+        $(".regular").slick({
+          dots: false,
+          infinite: true,
+          arrows:true,
+          slidesToShow: 4,
+          slidesToScroll: 1
+        });
+      });
+    </script>
+  </body>
+</html>
